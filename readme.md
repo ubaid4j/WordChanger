a455df1b6289ced6085a7a18c47a2c83e647a2ae
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-10 10:37:03 +0500

    configure

---
+++ readme.md
@@ -0,0 +1,5 @@
+<h2 align="center">Word Changer</h2>
+<ol>
+	<h3>How to run</3h>
+	
+</ol>
---
+++ src/main/java/com/ubaid/app/App.java
@@ -0,0 +1,9 @@
+package com.ubaid.app;
+
+public class App
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Will start soon");
+	}
+}
